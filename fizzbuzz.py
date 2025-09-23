3692af4fff797fd745ffc3ca01cf5f369180123c
kingwangzzang1234 <wychoi0408@naver.com>
2025-09-23 00:49:08 +0000

    fix: Rename Variable from i to k

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-for i in range(1, 15+1):
-    if i % 15 == 0:
+for k in range(5, 15+1):
+    if k % 15 == 0:
         print('fizzbuzz')
-    elif i % 3 == 0:
+    elif k % 3 == 0:
         print('fizz')
-    elif i % 5 == 0:
+    elif k % 5 == 0:
         print('buzz')
     else:
-        print(i)
+        print(k)
