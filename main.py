c3dd3180e1006b33af185e34fd9f5e60497af1cb
kingwangzzang1234 <wychoi0408@naver.com>
2026-02-05 05:17:24 +0000

    refactor: Do fizzbuzz with 1 if Statement

--- main.py
+++ main.py
@@ -3,12 +3,8 @@
 # 3의 배수, 'fizz'
 # 5의 배수, 'buzz'
 # 15의 배수, 'fizzbuzz'
-for i in range(1, 15+1):
-    if i % 15 == 0:
-        print('fizzbuzz')
-    elif i % 3 == 0:
-        print('fizz')
-    elif i % 5 == 0:
-        print('buzz')
+for i in range(15, 22+1):
+    if i % 3 == 0 or i % 5 == 0:
+        print('fizz' * (i%3==0) + 'buzz' * (i%5==0))
     else:
         print(i)
